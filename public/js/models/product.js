1a7b8c8dc598d3e54c5dfa57606eae8b7d0e8bf4
endor <fp@notjusthosting.com>
2011-06-24 12:20:07 -0700

    format unit price and valid to in product

--- public/js/models/product.js
+++ public/js/models/product.js
@@ -1,28 +1,66 @@
 karhu.Product = Backbone.Model.extend({
+  toJSON: function() {
+    return _.extend(_.clone(this.attributes), {
+      unit_price: this.parsedUnitPrice(),
+      valid_to: this._englishValidTo()
+    });
+  },
+
   toTemplate: function() {
     var category = this.category() || {toJSON: function() {}};
-    return _.extend(this.toJSON(), {category: category.toJSON()});
+
+    return _.extend(this.toJSON(), {
+      unit_price: this.formattedUnitPrice(),
+      valid_to: this.formattedValidTo(),
+      category: category.toJSON()
+    });
   },
   
   category: function() {
     return karhu.Categories.get(this.get('category_id'));
   },
   
+  parsedUnitPrice: function() {
+    var unit_price = this.get('unit_price');
+    if(unit_price && _.isString(unit_price)) {
+      return $.global.parseFloat(unit_price.match(/[\d\.\,]+/)[0]);
+    } else {
+      return unit_price;
+    }
+  },
+  
+  parsedValidTo: function() {
+    var valid_to = this.get('valid_to');
+    if(valid_to) {
+      return $.global.parseDate(valid_to) || Date.parse(valid_to);
+    } else {
+      return null;
+    }
+  },
+  
+  formattedUnitPrice: function() {
+    return $.global.format(this.parsedUnitPrice(), 'n') + '€';
+  },
+  
+  formattedValidTo: function() {
+    return $.global.format(this.parsedValidTo(), 'd');
+  },
+  
   validate: function(attrs) {
     var errors = {};
-    
+
     ['name', 'description', 'unit_price', 'valid_to'].forEach(function(attr) {
-      if(attrs[attr].length === 0) {
+      if(!attrs[attr] || attrs[attr].length === 0) {
         errors[attr] = karhu.i18n.translate('cannot_be_empty');
       }
     });
-    if(attrs.name.length > 100) {
+    if(attrs['name'] && attrs.name.length > 100) {
       errors['name'] = karhu.i18n.translate('cannot_be_longer_than_100_characters');
     }
-    if(!attrs.unit_price.match(this._regularExpression('unit_price'))) {
+    if(attrs['unit_price'] && !attrs.unit_price.match(this._regularExpression('unit_price'))) {
       errors['unit_price'] = karhu.i18n.translate('wrong_format_price');
     }
-    if(!attrs.valid_to.match(this._regularExpression('valid_to'))) {
+    if(attrs['valid_to'] && !attrs.valid_to.match(this._regularExpression('valid_to'))) {
       errors['valid_to'] = karhu.i18n.translate('wrong_format_date');
     }
     if(attrs.valid_to && !(Date.today().clearTime().compareTo(Date.parse(attrs.valid_to)) < 0)) {
@@ -43,5 +81,10 @@ karhu.Product = Backbone.Model.extend({
         valid_to: /\d{1,2}\.\d{1,2}\.\d{4}/
       }
     }[karhu.locale][field];
-  }  
+  },
+  
+  _englishValidTo: function() {
+    var valid_to = this.parsedValidTo();
+    return valid_to ? valid_to.toString('MM/dd/yyyy') : null;
+  }
 });
--- spec/javascripts/models/product_spec.js
+++ spec/javascripts/models/product_spec.js
@@ -1,62 +1,62 @@
 describe("Product", function() {
-  describe("attachCategory", function() {
+  beforeEach(function() {
+    karhu.i18n = { translate: function() {} };
+    karhu.locale = 'en';
+  });
+  
+  describe("toTemplate", function() {
     it("should assign itself its category", function() {
-      var categories = [{id: 1, name: 'Papiere'}, {id: 2, name: 'Baeume'}];
-      var attributes = {id: 1, name: 'Fichte', category_id: 2};
-      var product = new karhu.Product(attributes, categories);
-      expect(product.category.name).toEqual('Baeume');
+      var category = {toJSON: function() { return {id: 2, name: 'Baeume'}; } };
+      karhu.Categories = {
+        get: function() { return category; }
+      }
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2});
+      expect(product.toTemplate().category.name).toEqual('Baeume');
     });    
   });
   
-  describe("formatPrice", function() {
+  describe("formattedUnitPrice", function() {
     it("should add a € to the price", function() {
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, unit_price: 2.00};
-      var product = new karhu.Product(attributes);
-      expect(product.unit_price).toEqual('2.00€');
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2, unit_price: 2.00});
+      expect(product.formattedUnitPrice()).toEqual('2.00€');
     });
-
+  
     it("should format the price in german format if locale is german", function() {
       $.global.preferCulture('de');
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, unit_price: 2000.35};
-      var product = new karhu.Product(attributes);
-      expect(product.unit_price).toEqual('2.000,35€');
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2, unit_price: 2000.35});
+      expect(product.formattedUnitPrice()).toEqual('2.000,35€');
     });
-
+  
     it("should format the price in english format if locale is english", function() {
       $.global.preferCulture('en');
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, unit_price: 2000.35};
-      var product = new karhu.Product(attributes);
-      expect(product.unit_price).toEqual('2,000.35€');
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2, unit_price: 2000.35});
+      expect(product.formattedUnitPrice()).toEqual('2,000.35€');
     });    
   });
 
-  describe("formatValidTo", function() {
+  describe("formattedValidTo", function() {
     it("should format the date in german format if locale is german", function() {
       $.global.preferCulture('de');
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, valid_to: '12/10/2002'};
-      var product = new karhu.Product(attributes);
-      expect(product.valid_to).toEqual('10.12.2002');
+      var product = new karhu.Product({id: 1, name: 'Fichte', description: 'Baum', category_id: 2, valid_to: '12/10/2002'});
+      expect(product.formattedValidTo()).toEqual('10.12.2002');
     });
 
     it("should format the date in english format if locale is english", function() {
       $.global.preferCulture('en');
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, valid_to: '12/10/2002'};
-      var product = new karhu.Product(attributes);
-      expect(product.valid_to).toEqual('12/10/2002');
-    });    
+      var product = new karhu.Product({id: 1, name: 'Fichte', description: 'Baum', category_id: 2, valid_to: '12/10/2002'});
+      expect(product.formattedValidTo()).toEqual('12/10/2002');
+    });
   });
   
   describe("toJSON()", function() {
     it("should parse the price and dump it as float", function() {
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, unit_price: '2.00€', valid_to: '12/10/2002'};
-      var product = new karhu.Product(attributes);
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2, unit_price: '2.00€', valid_to: '12/10/2002'});
       expect(product.toJSON().unit_price).toEqual(2.00);
     });
     
     it("should parse the date and dump it as american date format", function() {
       $.global.preferCulture('de');
-      var attributes = {id: 1, name: 'Fichte', category_id: 2, valid_to: '10.12.2002'};
-      var product = new karhu.Product(attributes);
+      var product = new karhu.Product({id: 1, name: 'Fichte', category_id: 2, valid_to: '10.12.2002'});
       expect(product.toJSON().valid_to).toEqual('12/10/2002');
       $.global.preferCulture('en');
     });
